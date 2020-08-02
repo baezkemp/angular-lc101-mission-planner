127a41a9729cdd810875373ac1b4ca0c81b520b1
Liza Baez Kemp <baezkemp@gmail.com>
2020-08-01 22:02:02 -0500

    completed studio 2

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment:string[] = [
+    'Habitat dome',
+    'Drones',
+    'Food containers',
+    'Oxygen tanks'
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(piece: string) {
+    this.equipment.push(piece);
+  }
+
+  remove(piece: string) {
+    let index = this.equipment.indexOf(piece);
+    this.equipment.splice(index, 1);
+  }
+
+  equipmentBeingEdited: string[] = null;
+  edit(piece) {
+    this.equipmentBeingEdited = piece;
+  }
+
+  save(newEquipment: string, piece:string) {
+    let index = this.equipment.indexOf(piece);
+    this.equipment[index] = newEquipment;
+    this.equipmentBeingEdited = null;
   }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = [
+    'Mars soil sample',
+    'Plant growth in habitat',
+    'Human bone density'
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(hypothysis: string) {
+    this.experiments.push(hypothysis);
+  }
+
+  remove(hypothysis: string) {
+    let index = this.experiments.indexOf(hypothysis);
+    this.experiments.splice(index, 1);
+  }
+
+  experimentsBeingEdited: string[] = null;
+  edit(hypothysis) {
+    this.experimentsBeingEdited = hypothysis;
+  }
+
+  save(newExperiment: string, hypothysis:string) {
+    let index = this.experiments.indexOf(hypothysis);
+    this.experiments[index] = newExperiment;
+    this.experimentsBeingEdited = null;
   }
 
 }
